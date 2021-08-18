c7ba39e0fe11d151df478228b3dff127da3b8e20
MrPasquino <stuartjduffield@gmail.com>
2021-08-18 18:00:59 -0400

    Nearly done

--- unpack.py
+++ unpack.py
@@ -7,9 +7,9 @@ import os
 import glob
 
 def scan_log(inF,outF):
-    if os.path.exist(os.path.join(outF, 'scan.info')):
+    if os.path.isfile(os.path.join(outF, 'scan.info')):
         while True:
-            check = input('scan.info already exists./n Would you like to scan the dicom files anyway? /n This may take several minutes. (y/n)')
+            check = input('scan.info already exists. Would you like to scan the dicom files anyway? This may take several minutes. (y/n)')
             if check == 'y':
                 scan_log_cmd(inF,outF)
                 break
@@ -20,11 +20,25 @@ def scan_log(inF,outF):
 
 
 def scan_log_cmd(inF, outF):
-    cmd = 'dcmunpack -src %s -scanonly %s -index-out dcm.index.dat'%(inF,outF)
-    #call(cmd,shell=True)
-    print(cmd)
-    waitMsg = 'Please, wait, scanning can take a handful of minutes'
+
+    cmd = 'dcmunpack -src %s -scanonly %s -index-out %s'%(inF,os.path.join(outF,'scan.info'),os.path.join(outF,'dcm.index.dat'))
+    waitMsg = 'Please wait, scanning can take a handful of minutes'
     print(waitMsg)
+    call(cmd,shell=True)
+    print('Done')
+
+def unpack(inF,outF,runs,HDR):
+    runlist = ''
+    HDRs = ['mion','bold']
+    if HDR.lower() in HDRs:
+        for x in runs:
+            runlist += '\ -run %s %s nii f.nii'%(x,HDR)
+        end
+        cmd = 'dcmunpack -src . -index-in'
+            
+        
+    
+
 
 # def unpack_tsv():
 
